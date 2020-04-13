a1beac75694527de092eda453772900e72919257
gycphper <727306285@qq.com>
2020-04-13 16:35:48 +0800

    添加测试sql

--- data/user.sql
+++ data/user.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `mobile` char(32) NOT NULL,
+  `mobile` char(13) NOT NULL,
   `password` char(32) NOT NULL,
   `del_time` datetime NOT NULL,
   `is_ban` tinyint(4) NOT NULL DEFAULT '0',
@@ -9,4 +9,4 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='用户表'
 
 INSERT INTO api.`user` (mobile,password,del_time,is_ban,uuid) VALUES
-('15538177309','e10adc3949ba59abbe56e057f20f883e','2020-04-13 12:36:04.0',0,'');
+('15511111111','e10adc3949ba59abbe56e057f20f883e','2020-04-13 12:36:04.0',0,'');
